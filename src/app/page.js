24576dbdb818c87ed9cd73b9d26c1ef992eb3e65
Kristi Wu <kawu@usc.edu>
2024-10-27 16:42:39 -0700

    api

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,39 @@
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(question, answer);
+  }
+
   return (
-    <div>
-      Hello World
+    <div className="bg-black flex flex-col items-center justify-center h-screen">
+        <form className="flex flex-col gap-[10px]">
+          <h1>Question</h1>
+          <textarea 
+          className="w-96 h-24 p-2 rounded text-black"
+          placeholder="Enter your question"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <h1>Answer</h1>
+        <textarea 
+          className="w-96 h-24 p-2 rounded text-black"
+          placeholder="Enter your answer"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        />
+        <button 
+          type="submit" 
+          className="bg-white text-black px-4 py-2 rounded"
+        >
+          Submit
+        </button>
+        </form>
     </div>
   );
 }
