9f8b1240f99e9693b138ea494977677635a193e1
Kristi Wu <kawu@usc.edu>
2024-11-13 17:07:48 -0800

    teacher-side layout

---
+++ src/app/components/Teacher-Side/ProblemSpace.js
@@ -0,0 +1,8 @@
+
+export default function ProblemSpace() {
+    return(
+        <div className="h-screen w-[70%] text-black border-2 rounded-[12px] bg-[#FFF] border-[#D7D7D7] px-9 py-6 flex flex-col">
+            
+        </div>
+    );
+}
--- src/app/teacher-portal/page.js
+++ src/app/teacher-portal/page.js
@@ -2,14 +2,16 @@
 import '../globals.css';
 import ClassBar from '../components/Teacher-Side/ClassBar';
 import StudentList from '../components/Teacher-Side/StudentList';
+import ProblemSpace from '../components/Teacher-Side/ProblemSpace';
 
 export default function TeacherPortal() {
 
   return (
-    <div className="text-2xl text-black font-orienta h-auto bg-[#F8F7F4] px-12 pt-20 pb-12 flex flex-col gap-9">
+    <div className="text-2xl text-black font-orienta h-auto bg-[#F8F7F4] px-12 pt-20 pb-12 flex flex-col gap-3">
         <ClassBar />
-        <div className="flex">
+        <div className="flex gap-4">
             <StudentList />
+            <ProblemSpace />
         </div>
     </div>
   );
