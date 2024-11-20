b7d82a11b1293d5340b8420e230616c66aa6d046
Kristi Wu <kawu@usc.edu>
2024-11-19 21:30:27 -0800

    temporary adjustment to frontend

--- src/app/components/Student-Side/Question.js
+++ src/app/components/Student-Side/Question.js
@@ -1,6 +1,6 @@
 export default function Question({selectedQuestion}) {
     return(
-        <div className="h-[20%] font-bold text-2xl text-black border-2 rounded-[12px] border-[#D7D7D7] bg-[#FFF] px-9 py-6 flex flex-col justify-between gap-5 w-auto">
+        <div className="font-bold text-2xl text-black border-2 rounded-[12px] border-[#D7D7D7] bg-[#FFF] px-9 py-6 flex flex-col justify-between gap-5 w-auto">
             {selectedQuestion}
         </div>
     );
