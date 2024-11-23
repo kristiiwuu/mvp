e6b42f1c1877962243c031ab8dd3c7b7993947f8
Kristi Wu <kawu@usc.edu>
2024-11-22 20:48:18 -0800

    correctness indicator

--- src/app/components/Student-Side/TextBubble.js
+++ src/app/components/Student-Side/TextBubble.js
@@ -1,11 +1,5 @@
-import { useEffect } from "react";
-
 export default function TextBubble(props) {
-    useEffect(() => {
-        console.log(props);
-    }, [])
-    // gray bubble for duey's responses; blue bubble for user 
-    return(
+    return (
         <div ref={props.ref} className={`m-0 flex ${props.message.role === 'user' ? 'justify-end' : 'justify-start'}`}>
             <div className={`rounded-[12px] px-8 py-5 m-0 max-w-[70%] ${props.message.role === 'user' ? 'bg-[#D5E6ED] text-[#084762]' : 'bg-gray-200'}`}>
                 {props.message.content}
