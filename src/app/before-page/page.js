39027f39058a35af1a68b8637d675f0fe46c74a2
Kristi Wu <kawu@usc.edu>
2024-12-03 21:33:45 -0800

    fix

--- src/app/before-page/page.js
+++ src/app/before-page/page.js
@@ -6,21 +6,25 @@ import { useSearchParams } from 'next/navigation'
 
 export default function StudentPortal() {
 
-    const searchParams = useSearchParams();
-    const title = searchParams.get('title');
-    const dueDate = searchParams.get('dueDate');
-    const dueTime = searchParams.get('dueTime');
-    const startDate = searchParams.get('startDate');
-    const startTime = searchParams.get('startTime');
-    const endDate = searchParams.get('endDate');
-    const endTime = searchParams.get('endTime');
-    const submitted = searchParams.get('submitted') === "true";
-    const num = searchParams.get('num');
-
-
-
     return(
         <React.Suspense fallback={<div>Loading...</div>}>
+            <SearchParamsWrapper />
+        </React.Suspense>
+    );
+
+    function SearchParamsWrapper() {
+        const searchParams = useSearchParams();
+        const title = searchParams.get('title');
+        const dueDate = searchParams.get('dueDate');
+        const dueTime = searchParams.get('dueTime');
+        const startDate = searchParams.get('startDate');
+        const startTime = searchParams.get('startTime');
+        const endDate = searchParams.get('endDate');
+        const endTime = searchParams.get('endTime');
+        const submitted = searchParams.get('submitted') === "true";
+        const num = searchParams.get('num');
+    
+        return (
             <div className="font-orienta w-auto bg-[#F8F7F4] h-screen flex flex-col pb-12 items-center gap-10">
                 <Header title={title}/>
                 <AssignmentCard 
@@ -35,6 +39,25 @@ export default function StudentPortal() {
                     num={num}
                 />
             </div>
-        </React.Suspense>
-    );
+        );
+    }
+
+    // return(
+    //     <React.Suspense fallback={<div>Loading...</div>}>
+    //         <div className="font-orienta w-auto bg-[#F8F7F4] h-screen flex flex-col pb-12 items-center gap-10">
+    //             <Header title={title}/>
+    //             <AssignmentCard 
+    //                 title={title} 
+    //                 dueDate={dueDate} 
+    //                 dueTime={dueTime} 
+    //                 startDate={startDate} 
+    //                 startTime={startTime} 
+    //                 endDate={endDate} 
+    //                 endTime={endTime} 
+    //                 submitted={submitted} 
+    //                 num={num}
+    //             />
+    //         </div>
+    //     </React.Suspense>
+    // );
 }
