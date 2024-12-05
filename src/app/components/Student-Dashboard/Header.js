b7859898e156703157ee6dc8a602f43ccee8fa7c
Kristi Wu <kawu@usc.edu>
2024-12-05 01:42:36 -0800

    installed tailwindcss-animate; font white

--- src/app/components/Student-Dashboard/Header.js
+++ src/app/components/Student-Dashboard/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return(
         <div className="font-semibold bg-[#1F8FBF] w-auto h-[15%] text-3xl flex text-center items-center p-5">
             <Home className="scale-90"/>
-            <div className="ml-4">
+            <div className="ml-4 text-white">
                 BIOLOGY - Period 5
             </div>
         </div>
