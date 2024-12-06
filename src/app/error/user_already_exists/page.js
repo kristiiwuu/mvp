29198811797355b1f270fc137d745cc38a286639
Kristi Wu <kawu@usc.edu>
2024-12-05 17:21:05 -0800

    added return to login button

--- src/app/error/user_already_exists/page.js
+++ src/app/error/user_already_exists/page.js
@@ -1,8 +1,21 @@
+"use client"
+import { redirect } from 'next/navigation'
+
 export default function ErrorPage() {
-    return (
-      <div className="font-orienta h-screen bg-[#F8F7F4] text-[#1F8FBF] text-3xl flex justify-center items-center">
-         <p>User already exists. Try logging in.</p>
-      </div>
-    );
-  }
-  
+
+  const handleClick = () => {
+    redirect('/')
+}
+
+  return (
+    <div className="font-figtree h-screen bg-[#F8F7F4] text-[#1F8FBF] text-3xl flex flex-col justify-center items-center">
+       <p>User already exists. Try logging in.</p>
+       <button className='mt-5 flex w-[200px] px-2 py-2 justify-center items-center bg-[#1F8FBF] text-white rounded-[6px] hover:bg-[#58B6DF]'
+        onClick={handleClick}>
+          Return
+        </button>
+    </div>
+  );
+}
+
+
